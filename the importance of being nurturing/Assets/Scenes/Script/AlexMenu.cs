cedc32486ef0d410762ea2adf27244e050d87c52
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2019-11-04 09:57:33 +1100

    g

---
+++ the importance of being nurturing/Assets/Scenes/Script/AlexMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class AlexMenu : MonoBehaviour
+{
+    public menuSelect MenuSelect = menuSelect.Play;
+    public GameObject SettingsGO;
+
+    public void ClickButton()
+    {
+
+        switch (MenuSelect)
+        {
+            case menuSelect.Play:
+                break;
+            case menuSelect.Settings:
+                Setting.SettingsOpen = !Setting.SettingsOpen;
+                break;
+            case menuSelect.Quit:
+                Application.Quit();
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Setting.SettingsOpen)
+        {
+            SettingsGO.SetActive(true);
+        }
+        else if (!Setting.SettingsOpen)
+        {
+            SettingsGO.SetActive(false);
+        }
+    }
+
+    public enum menuSelect
+    {
+        Play,
+        Settings,
+        Quit
+    };
+}
---
+++ the importance of being nurturing/Assets/Scenes/Script/DialogueOptions.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueOptions : MonoBehaviour
+{
+    public string[] text;
+    public int index, option;
+    public bool showDlg;
+    private void OnGUI()
+    {
+        if (showDlg)
+        {
+            Vector2 scr = new Vector2(Screen.width / 16, Screen.height / 9);
+            //makes half the screen into a dialogue box
+            GUI.Box(new Rect(0, scr.y * 6, Screen.width, scr.y * 3), text[index]);
+            if (!(index >= text.Length - 1 || index == option))
+            {
+                //reads the different lines in order
+                if (GUI.Button(new Rect(scr.x * 14, scr.y * 8, scr.x * 2, scr.y), "Next"))
+                {
+                    index++;
+                }
+            }
+            else if (index == option)
+            {
+                //makes you end the dialogue early
+                if (GUI.Button(new Rect(scr.x * 14, scr.y * 8, scr.x * 2, scr.y), "skip"))
+                {
+                    index = text.Length - 1;
+                }
+                //continues the dialogue in order
+                if (GUI.Button(new Rect(scr.x * 10, scr.y * 8, scr.x * 2, scr.y), "Next"))
+                {
+                    index++;
+                }
+            }
+            else
+            {
+                //allows you to leave after the dialogue is over 
+                if (GUI.Button(new Rect(scr.x * 14, scr.y * 8, scr.x * 2, scr.y), "Bye"))
+                {
+                    index = 0;
+                    showDlg = false;
+                }
+            }
+        }
+    }
+}
